4429fb70ea8bf6c872109ee32de6dbbecc55b71b
Tobias Althoff <dev@tobiasalthoff.de>
2020-06-17 16:55:43 +0200

    Add theme test files

---
+++ test/javascript.js
@@ -0,0 +1,68 @@
+// 1. Example
+
+var Sales = "Toyota";
+
+function CarTypes(name) {
+  if (name == "Honda") {
+    return name;
+  } else {
+    return "Sorry, we don't sell " + name + ".";
+  }
+}
+
+var car = { myCar: "Saturn", getCar: CarTypes("Honda"), special: Sales };
+
+console.log(car.special);
+
+// 2. Tests
+
+import Test, { nothing } from 'nothing';
+
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+
+  if (price < 0) {
+    throw RangeError('Cannot create product ' + this.name + ' with a negative price');
+  }
+}
+
+function Food(name, price) {
+  Product.call(this, name, price);
+  this.category = 'food';
+}
+
+function Toy(name, price) {
+  Product.call(this, name, price);
+  this.category = 'toy';
+}
+
+export const cake = new Food('choc', 0.5);
+let soda = new Food('cola', 5.95);
+var cheese = new Food('feta', 5);
+var fun = new Toy('robot', 40);
+
+/**
+sample javascript from xui
+*/
+
+var undefined,
+    xui,
+    window     = this,
+    string     = new String('string'),
+    document   = window.document,
+    simpleExpr = /^#?([\w-]+)$/,
+    idExpr     = /^#/,
+    tagExpr    = /<([\w:]+)/,
+    slice      = function (e) { return [].slice.call(e, 0); };
+    try { var a = slice(document.documentElement.childNodes)[0].nodeType; }
+    catch(e){ slice = function (e) { var ret=[]; for (var i=0; e[i]; i++)
+        ret.push(e[i]); return ret; }; }
+
+window.x$ = window.xui = xui = function(q, context) {
+    return new xui.fn.find(q, context);
+};
+
+module.exports = Food;
+
+export default Toy;
